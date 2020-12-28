789cb284b722a82d7506ff53bf89ca8538f4eb71
John Clark <johnnyclark90@gmail.com>
2020-12-28 15:16:10 -0800

    cleaned up formatting

--- euler001.py
+++ euler001.py
@@ -2,6 +2,7 @@
 
 
 def calculate(below):
+    """Returns the sum of all the multiples of 3 or 5 below the given number"""
     answer = 0
     for number in range(below):
         if not (number % 3 and number % 5):
--- euler002.py
+++ euler002.py
@@ -1,11 +1,19 @@
 """Project Euler problem 2"""
-TERM_1 = 1
-TERM_2 = 2
-SUM = 0
-while TERM_2 < 4000000:
-    if not TERM_2 % 2:
-        SUM += TERM_2
-    NEXT_TERM = TERM_1 + TERM_2
-    TERM_1 = TERM_2
-    TERM_2 = NEXT_TERM
-print(SUM)
+
+
+def calculate(not_to_exceed):
+    """Returns the sum of the even-valued terms in the Fibonacci sequence
+    whose values do not exceed the given number"""
+    term_one = 1
+    term_two = 2
+    total = 0
+    while term_two < not_to_exceed:
+        if not term_two % 2:
+            total += term_two
+        next_term = term_one + term_two
+        term_one = term_two
+        term_two = next_term
+    return total
+
+
+print(calculate(4000000))
