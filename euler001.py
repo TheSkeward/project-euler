5de0d71af92208140b4ba60326f372a553cfa6a0
John Clark <johnnyclark90@gmail.com>
2020-12-28 15:11:41 -0800

    cleaned up formatting

--- euler001.py
+++ euler001.py
@@ -1,6 +1,12 @@
 """Project Euler problem 1"""
-ANSWER = 0
-for number in range(1000):
-    if not (number % 3 and number % 5):
-        ANSWER += number
-print(ANSWER)
+
+
+def calculate(below):
+    answer = 0
+    for number in range(below):
+        if not (number % 3 and number % 5):
+            answer += number
+    return answer
+
+
+print(calculate(1000))
