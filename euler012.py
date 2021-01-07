1447e7a70ea26166de74a2248333d48e5fbe8760
John Clark <johnnyclark90@gmail.com>
2021-01-07 15:06:19 -0800

    Problem 12

---
+++ euler012.py
@@ -0,0 +1,26 @@
+"""Project Euler problem 12"""
+from euler003 import sqrt
+
+
+def num_divisors(number):
+    """Returns the number of divisors of the specified number"""
+    end = sqrt(number)
+    result = sum(2 for i in range(1, end + 1) if number % i == 0)
+    return result
+
+
+def calculate(over):
+    """Returns the value of the first triangle number to have
+    over the specified number of divisors"""
+    triangle = 0
+    answer = 0
+    count = sum(range(triangle))
+    while True:
+        if num_divisors(count) > over:
+            answer = count
+            return answer
+        triangle += 1
+        count = sum(range(triangle))
+
+
+print(calculate(500))
